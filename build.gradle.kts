8df03205b23a911eb415e6b728530487541e1aee
jarekratajski <jratajski@gmail.com>
2021-05-06 19:45:39 +0200

    added build

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    kotlin("jvm") version "1.4.31"
+    id("io.gitlab.arturbosch.detekt").version("1.16.0")
+}
+
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    jcenter()
+    maven { url = uri("https://dl.bintray.com/arrow-kt/arrow-kt/") }
+}
+
+val ktor_version = "1.5.1"
+val nee_version = "0.7.0"
+
+dependencies {
+    implementation("pl.setblack:nee-ctx-web-ktor:$nee_version")
+    detektPlugins("pl.setblack:kure-potlin:0.5.0")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.16.0")
+    implementation("io.ktor:ktor-http-jvm:$ktor_version")
+    implementation("io.ktor:ktor-jackson:$ktor_version")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-parameter-names:2.12.0")
+
+    testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.4.3")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:4.4.3")
+    testImplementation("pl.setblack:nee-ctx-web-test:$nee_version")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+
+val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
+compileKotlin.kotlinOptions.apply {
+    jvmTarget = "1.8"
+    javaParameters = true
+    allWarningsAsErrors = true
+    freeCompilerArgs = listOf("-XXLanguage:+InlineClasses")
+}
+
+val compileTestKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
+compileTestKotlin.kotlinOptions.apply {
+    jvmTarget = "1.8"
+    javaParameters = true
+    allWarningsAsErrors = true
+    freeCompilerArgs = listOf("-XXLanguage:+InlineClasses")
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    this.jvmTarget = "1.8"
+    this.classpath.setFrom(compileKotlin.classpath.asPath)
+}
+
+tasks {
+    "build" {
+        dependsOn("detektMain")
+    }
+}
--- src/test/kotlin/dev/neeffect/example/todo/HelloTest.kt
+++ src/test/kotlin/dev/neeffect/example/todo/HelloTest.kt
@@ -9,17 +9,15 @@ import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.withTestApplication
-import java.time.LocalDateTime
-import java.time.ZoneOffset
 
 class HelloTest : StringSpec({
     "server should return hello" {
-        withTestApplication (rest )  {
+        withTestApplication(rest) {
             with(handleRequest(HttpMethod.Get, "/hello")) {
                 response.content shouldBe "\"hello world\""
             }
         }
-        withTestApplication (rest )  {
+        withTestApplication(rest) {
             with(handleRequest(HttpMethod.Get, "/error")) {
                 response.status() shouldBe HttpStatusCode.NotFound
             }
@@ -27,8 +25,6 @@ class HelloTest : StringSpec({
     }
 }) {
     companion object {
-        val constTime = suspend { LocalDateTime.parse("2021-03-14T10:10:10").toInstant(ZoneOffset.UTC) }
-
         val rest: Application.() -> Unit = testApplication(
             DefaultJacksonMapper.mapper,
             webContextProvider
